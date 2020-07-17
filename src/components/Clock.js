f32ca94707b63fd2e5797f8b28c19f2af5895625
johnny <jbui923@gmail.com>
2020-07-17 14:08:31 -0700

    date and clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -24,17 +24,19 @@ class Clock extends React.Component {
     let hours = this.state.time.getHours();
     let minutes = this.state.time.getMinutes();
     let seconds = this.state.time.getSeconds();
+
   
     hours = (hours < 10) ? `0${hours}` : hours;
     minutes = (minutes < 10) ? `0${minutes}` : minutes;
     seconds = (seconds < 10) ? `0${seconds}` : seconds;
+   
 
     return (
       <>
         <div className='header__clock'>
           <p>
             <span>
-                {this.state.time.toDateString()} {hours}:{minutes}:{seconds} LA
+                {this.state.time.toLocaleDateString()} {this.state.time.toLocaleTimeString()} LA
             </span>
           </p>
         </div>
